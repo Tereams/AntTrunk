8b31bf9ab7d554983492d53e04067928508e64e4
tereams <tianyue.chen@outlook.com>
2026-05-13 22:03:42 -0700

    Update Account.h

--- include/Account.h
+++ include/Account.h
@@ -1,12 +1,35 @@
-#pragma once
+#ifndef ANTTRUNK_ACCOUNT_HPP
+#define ANTTRUNK_ACCOUNT_HPP
 
 #include <string>
 
-struct Account {
-    std::string name;
-    double initial_balance;
-    Account(
-        const std::string& name,
-        double initial_balance
-    );
+namespace anttrunk {
+
+enum class AccountType {
+    Checking,
+    Savings,
+    CreditCard,
+    Cash,
+    Investment,
+    Other
 };
+
+struct Account {
+    std::string id;
+
+    std::string name;
+    std::string bank_name;
+    AccountType type = AccountType::Other;
+
+    std::string currency = "USD";
+
+    // The starting balance recorded by the user.
+    // Current balance should be calculated from this value plus transactions.
+    double initial_balance = 0.0;
+
+    std::string description;
+};
+
+} // namespace anttrunk
+
+#endif // ANTTRUNK_ACCOUNT_HPP
