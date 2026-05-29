83aed07662a4778f42da15241da34097701f084a
tereams <tianyue.chen@outlook.com>
2026-05-28 23:49:51 -0700

    Update finance_app.cpp

--- src/finance_app.cpp
+++ src/finance_app.cpp
@@ -74,3 +74,74 @@ void FinanceApp::handleOption(int option) {
             break;
     }
 }
+
+
+void FinanceApp::addAccount() {
+    Account account;
+
+    std::cout << "Enter account id: ";
+    std::getline(std::cin, account.id);
+
+    std::cout << "Enter account name: ";
+    std::getline(std::cin, account.name);
+
+    std::cout << "Enter bank name: ";
+    std::getline(std::cin, account.bank_name);
+
+    std::cout << "Select account type:\n";
+    std::cout << "1. Checking\n";
+    std::cout << "2. Savings\n";
+    std::cout << "3. Credit Card\n";
+    std::cout << "4. Cash\n";
+    std::cout << "5. Investment\n";
+    std::cout << "6. Other\n";
+    std::cout << "Choice: ";
+
+    int type_choice;
+    std::cin >> type_choice;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    switch (type_choice) {
+        case 1:
+            account.type = AccountType::Checking;
+            break;
+        case 2:
+            account.type = AccountType::Savings;
+            break;
+        case 3:
+            account.type = AccountType::CreditCard;
+            break;
+        case 4:
+            account.type = AccountType::Cash;
+            break;
+        case 5:
+            account.type = AccountType::Investment;
+            break;
+        case 6:
+            account.type = AccountType::Other;
+            break;
+        default:
+            std::cout << "Invalid choice. Set to Other by default.\n";
+            account.type = AccountType::Other;
+            break;
+    }
+
+    std::cout << "Enter currency [default: USD]: ";
+    std::string currency;
+    std::getline(std::cin, currency);
+
+    if (!currency.empty()) {
+        account.currency = currency;
+    }
+
+    std::cout << "Enter initial balance: ";
+    std::cin >> account.initial_balance;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::cout << "Enter description: ";
+    std::getline(std::cin, account.description);
+
+    accounts.push_back(account);
+
+    std::cout << "Account added successfully.\n";
+}
