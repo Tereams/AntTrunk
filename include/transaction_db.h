a1501c9fd244a9e30e7cc8721213ab0a1587da50
tereams <tianyue.chen@outlook.com>
2026-06-19 23:53:52 -0700

    Create transaction_db.h

---
+++ include/transaction_db.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "transaction.h"
+
+#include <sqlite3.h>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace transaction_db {
+
+bool create_table(sqlite3* db);
+
+bool insert(sqlite3* db, const Transaction& transaction);
+
+std::optional<Transaction> find_by_id(sqlite3* db, const std::string& id);
+
+std::vector<Transaction> get_all(sqlite3* db);
+
+bool update(sqlite3* db, const Transaction& transaction);
+
+bool remove(sqlite3* db, const std::string& id);
+
+}
