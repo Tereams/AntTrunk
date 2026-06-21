b67b1988cf2b99cdad105cefe4d8acbe12add63c
tereams <tianyue.chen@outlook.com>
2026-06-21 15:34:02 -0700

    Create recurring_transaction_db.h

---
+++ include/recurring_transaction_db.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "recurring_transaction.h"
+
+#include <sqlite3.h>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace recurring_transaction_db {
+
+bool create_table(sqlite3* db);
+
+bool insert(sqlite3* db, const RecurringTransaction& recurring);
+
+std::optional<RecurringTransaction> find_by_id(sqlite3* db, const std::string& id);
+
+std::vector<RecurringTransaction> get_all(sqlite3* db);
+
+bool update(sqlite3* db, const RecurringTransaction& recurring);
+
+bool remove(sqlite3* db, const std::string& id);
+
+}
